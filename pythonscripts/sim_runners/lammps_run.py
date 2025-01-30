63938441d44a9d367935f29d6aed6b7498b3adfd
heidi_susanna <heidi-susanna.laitinen@helsinki.fi>
2025-01-30 16:40:56 +0200

    Update paths and readme

--- pythonscripts/sim_runners/lammps_run.py
+++ pythonscripts/sim_runners/lammps_run.py
@@ -3,13 +3,13 @@ from mpi4py import MPI
 
 """Script to test running LAMMPS from python"""
 
-# Run parallel using mpirun -np N python3 lammps_run.py
+# Run parallel using mpirun -np N python3 pythonscripts/sim_runners/lammps_run.py
 
 lmp = lammps()
 # lmp.file("input_scripts/in.10nm_flux_1358")
-# lmp.file("input_scripts/in.28x28x17_relaxation")
+lmp.file("lammps/relaxation_scripts/in.relaxation")
 # lmp.file("input_scripts/in.heat_center")
-lmp.file("lammps/input_scripts/in.rescale_py")
+# lmp.file("lammps/input_scripts/in.rescale_py")
 
 me = MPI.COMM_WORLD.Get_rank()
 nprocs = MPI.COMM_WORLD.Get_size()
