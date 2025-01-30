41e0a42c49a13a5fe84a25b45ce4e52e4357ce3c
heidi_susanna <heidi-susanna.laitinen@helsinki.fi>
2025-01-30 15:57:37 +0200

    Update .gitignore and readme

--- pythonscripts/sim_runners/lammps_run.py
+++ pythonscripts/sim_runners/lammps_run.py
@@ -9,7 +9,7 @@ lmp = lammps()
 # lmp.file("input_scripts/in.10nm_flux_1358")
 # lmp.file("input_scripts/in.28x28x17_relaxation")
 # lmp.file("input_scripts/in.heat_center")
-lmp.file("input_scripts/in.rescale_py")
+lmp.file("lammps/input_scripts/in.rescale_py")
 
 me = MPI.COMM_WORLD.Get_rank()
 nprocs = MPI.COMM_WORLD.Get_size()
